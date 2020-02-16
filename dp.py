64ef23776eb259a556a343d3b79b09e5b689b161
Larry Lai <dppss91102@gmail.com>
2020-02-16 16:19:51 -0500

    Add dp solution of practice problem

---
+++ dp.py
@@ -0,0 +1,25 @@
+def solve_pizza(nums, target):
+    dp = [[None for _ in range(len(nums))] for _ in range(target+1)]
+
+    dp[nums[0]][0] = [nums[0]]
+
+    for j in range(len(nums)):
+        dp[0][j] = []
+
+    for i in range(target+1):
+        for j in range(len(nums)):
+            if j-1 >= 0:
+                if dp[i][j-1] is not None:
+                    dp[i][j] = dp[i][j-1]
+                    continue
+                if i-nums[j] >= 0:
+                    if dp[i-nums[j]][j-1] is not None:
+                        dp[i][j] = list(dp[i-nums[j]][j-1] + [nums[j]])
+    
+    for i in range(target, -1, -1):
+        if dp[i][-1] is not None:
+            return dp[i][-1]
+    
+if __name__ == '__main__':
+    solve_pizza([2, 5, 6, 8], 17)
+    
