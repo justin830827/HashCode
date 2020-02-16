0f418265b821f9f0c0f1b8110864db0baa9e5a9f
Justin Hu <justin801514@gmail.com>
2020-02-16 16:45:03 -0500

    main function

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # HashCode
+
+## How to run this code
+
+```
+python3 main.py (filename without extension)
+```
---
+++ main.py
@@ -0,0 +1,37 @@
+import sys
+from dp import *
+
+
+def read_file(filename):
+    path = './Data/' + filename + '.in'
+    with open(path, 'r') as f:
+        spec = f.readline().split()
+        target, number = int(spec[0]), int(spec[1])
+        candidates = list(map(int, f.readline().split()))
+
+    return target, number, candidates
+
+
+def wirte(results, filename):
+    path = './Output/' + filename + '.txt'
+    with open(filename, 'w') as f:
+        f.write('{}\n'.format(len(results)))
+        for res in results:
+            f.write(str(res) + ' ')
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("No input file !!")
+        exit()
+
+    filename = sys.argv[1]
+
+    target, number, candidates = read_file(filename)
+
+    res = solve_pizza(candidates, target)
+    wirte(res, filename)
+
+
+if __name__ == '__main__':
+    main()
